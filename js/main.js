383b357fd9b5b2e48adb7581e63b4b1eee338c2f
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2022-05-20 22:34:50 -0300

    commit 2: primera estructura creada. En rama paginaBase

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//cambiar el color del logo y los íconos al abrir el menú
+let logo = document.querySelector(".logoSVG");
+let menu = document.querySelector("#menuToggle");
+let colorCambiado = 1;
+menu.addEventListener("click", cambiarColor);
+function cambiarColor()
+{
+    if(colorCambiado == 1)
+    {
+        document.querySelectorAll(".fa").forEach(e => e.style.color = "white");
+        logo.style.filter = "invert(95%) sepia(59%) saturate(14%) hue-rotate(298deg) brightness(105%) contrast(103%)";
+        colorCambiado = 0;
+    }
+    else
+    {
+        document.querySelectorAll(".fa").forEach(e => e.style.color = "#7b9a57");
+        logo.style.filter = "invert(56%) sepia(19%) saturate(809%) hue-rotate(45deg) brightness(97%) contrast(91%)";
+        colorCambiado = 1;
+    }
+}
