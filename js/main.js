fbd30ec65718884764e1cb4d166904d74a7da124
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2022-06-22 23:18:37 -0300

    commit 5: proyecto casi concluido. Faltan agregar imágenes finales, empresas que cooperan con Cus Cus, corregir error de desplazamiento en Y, error del footer y enlazar el formulario al correo oficial

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //cambiar el color del logo y los íconos al abrir el menú
-let logo = document.querySelector(".logoSVG");
+//let logo = document.querySelector(".logoSVG");
+let logo = document.querySelector("#SvgjsG1008");
+let circle = document.querySelector(".strokeChanged");
+let string = document.querySelector(".stringColor");
 let menu = document.querySelector("#menuToggle input");
 let colorCambiado = 1;
 menu.addEventListener("click", cambiarColor);
@@ -8,13 +11,20 @@ function cambiarColor()
     if(colorCambiado == 1)
     {
         document.querySelectorAll(".fajs").forEach(e => e.style.color = "#f5f5dc");
-        logo.style.filter = "invert(91%) sepia(19%) saturate(196%) hue-rotate(22deg) brightness(106%) contrast(92%)";
+        logo.style.fill = "#f5f5dc";
+        document.querySelectorAll(".colorChanged").forEach(f => f.style.fill = "#f5f5dc");
+        circle.style.stroke = "#f5f5dc"
+        string.style.stroke = "#f5f5dc"
+
         colorCambiado = 0;
     }
     else
     {
         document.querySelectorAll(".fajs").forEach(e => e.style.color = "#7b9a57");
-        logo.style.filter = "invert(56%) sepia(19%) saturate(809%) hue-rotate(45deg) brightness(97%) contrast(91%)";
+        logo.style.fill = "#7b9a57";
+        document.querySelectorAll(".colorChanged").forEach(f => f.style.fill = "#7b9a57");
+        circle.style.stroke = "#7b9a57"
+        string.style.stroke = "#7b9a57"
         colorCambiado = 1;
     }
 }
