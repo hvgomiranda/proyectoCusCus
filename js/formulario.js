e92a5dad49152f9bf3e6caa34b4e76c343b4ca47
agiongrande <agiongrande@gmail.com>
2022-06-26 17:15:21 -0300

    ajustes finales

--- js/formulario.js
+++ js/formulario.js
@@ -1,21 +1,22 @@
-const $form = document.querySelector("#form");
-$form.addEventListener("submit", handleSubmit);
+const $form = document.querySelector("#form")
+
+$form.addEventListener("submit", handleSubmit)
 
 async function handleSubmit(event)
 {
-    event.preventDefault();
-    const form = new FormData(this);
+    event.preventDefault()
+    const form = new FormData(this)
     const response = await fetch(this.action, {
         method: this.method,
         body: form,
-        header: {
+        headers: {
             "Accept": "application/json"
         }
     })
     if(response.ok)
     {
-        this.reset;
-        alertMsg("¡Gracias por contactarnos!", "green");
+        this.reset()
+        alertMsg("¡Gracias por contactarnos!", "green")
     }
 }
 
